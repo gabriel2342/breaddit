9bf226862602df1a70622e8edfa6845ac80f93c6
gabriel2342 <gabriel.pendas@gmail.com>
2022-04-06 17:04:18 -0600

    finishing breaddit CRUD

--- app/controllers/breads_controller.rb
+++ app/controllers/breads_controller.rb
@@ -5,5 +5,44 @@ class BreadsController < ApplicationController
     @breads = Bread.all 
   end
 
+  def show; end
+
+  def new
+    @bread = Bread.new 
+  end
+
+  def create 
+    @bread = Bread.new(bread_params)
+
+    if bread.save 
+      redirect_to breads_path, notice: "Bread was successfully baked."
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update 
+    if @bread.update(bread_params)
+      redirect_to breads_path, notice: "Rebake was successful."
+    end
+  end
+
+  def destroy
+    @bread.destory
+    redirect_to breads_path, notice: "Your bread is no more."
+  end
+
+  private
+
+  def set_bread
+    @bread = Bread.find(params[:id])
+  end
+
+  def bread_params
+    params.require(:bread).permit(:bread_name, :bread_story, :baker_id)
+  end
+
 
 end
